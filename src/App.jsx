19a0ad9864f10a902025353a23307401e25abcb6
Jhon Álvarez <jwalvarez.98@gmail.com>
2022-05-07 21:19:16 -0500

    getting products from fake api, added props to product component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Product from "./components/Product";
+import axios from "axios";
 
 const App = () => {
+  useEffect(() => {
+    getProducts();
+
+    return () => {
+      console.log("second");
+    };
+  }, []);
+
+  const [products, setProducts] = useState([]);
+
+  var config = {
+    method: "get",
+    url: "https://api.steinhq.com/v1/storages/62759933bca21f053e914897/products",
+    headers: {
+      Authorization: "Basic c2hvcDpxd2VydHk=",
+    },
+  };
+
+  const getProducts = () => {
+    axios(config)
+      .then(function (response) {
+        setProducts(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="bg-gray-50 w-100 h-screen p-10">
       <span className="flex">
@@ -10,11 +39,18 @@ const App = () => {
           🥝
         </a>
       </span>
-      <section className="flex">
-        <Product />
-        <Product />
-        <Product />
-      </section>
+      <div className="w-auto my-auto flex flex-nowrap flex-row justify-start overflow-auto">
+        {products.map((product) => (
+          <Product
+            key={product["sku"]}
+            productName={product["product_name"]}
+            productShortDescription={product["short_description"]}
+            price={product["price"]}
+            imageUrl={product["product_image"]}
+            category={product["category_1"]}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 
-const Product = () => {
+const Product = ({
+  productName,
+  productShortDescription,
+  imageUrl,
+  price,
+  category,
+}) => {
   return (
-    <div className="bg-[rgb(250,250,250)] w-72 rounded hover:drop-shadow-[0px_6px_rgba(24,24,24,1)] outline outline-1 outline-gray-200 mr-6 transition-all duration-300">
-      <figure className="relative">
+    <div className="bg-[rgb(250,250,250)] w-64 rounded hover:drop-shadow-[0px_6px_rgba(24,24,24,1)] outline outline-1 outline-gray-200 mr-6 mb-6 transition-all duration-300">
+      <figure className="relative overflow-hidden">
         <a
-          className="absolute right-0 text-xs py-1 px-6 m-2 hover:bg-white bg-gray-50/90 rounded border-2 border-gray-300"
+          className="z-10 absolute right-0 text-xs py-1 px-6 m-2 hover:bg-white bg-gray-50/90 rounded border-2 border-gray-300"
           href="http://"
         >
-          Best seller
+          {category}
         </a>
-        <a className="absolute bottom-0 w-full text-xs py-1 px-6 mx-auto my-1">
+        <a className="z-10 absolute bottom-0 w-full text-xs py-1 px-6 mx-auto my-1">
           <div className="flex justify-center cursor-default">
             <div className="w-2 h-2 my-auto mx-2 bg-indigo-300 rounded-full border-2 border-indigo-400"></div>
             Original
           </div>
         </a>
         <img
-          className="rounded-t cursor-pointer"
-          src="https://static-assets.glossier.com/production/spree/images/attachments/000/003/001/portrait_normal/Lavender-BDC_hover.jpg?1651582497=&w=600&q=80&fm=webp"
+          className="rounded-t cursor-pointer hover:scale-110 transition-all duration-300"
+          src={imageUrl}
           alt="Imagen de producto"
         />
       </figure>
       <div className="p-2">
         <a href="http://" className="hover:underline hover:text-indigo-800">
-          <p className="font-bold text-lg mb-0 pb-0">Balm Dotcom</p>
+          <p className="font-bold text-lg mb-0 pb-0">{productName}</p>
           <p className="text-sm text-gray-700 pt-0 mt-0">
-            universal skin salve
+            {productShortDescription}
           </p>
         </a>
         <div className="flex justify-start h-8">
@@ -35,7 +41,7 @@ const Product = () => {
           <div className="w-5 h-5 my-auto mr-2 bg-blue-300 rounded-full border-2 hover:border-indigo-400 cursor-pointer"></div>
         </div>
         <button className="bg-black/95 rounded w-full py-3 mt-4 text-white text-sm font-medium hover:bg-black/90">
-          Agregar - $12.000
+          Agregar - ${price}
         </button>
       </div>
     </div>
